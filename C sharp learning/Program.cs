cc9ada18e6158a59daedcb5bfed838af3d5bf76e
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-09-03 16:37:22 +0400

    Add files via upload

---
+++ C sharp learning/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic; //массив, списки.
+using System.ComponentModel;
+using System.Globalization;
+using System.Net;
+
+namespace C_sharp_learning
+{
+
+    interface ISomeInterface
+    {
+        int property { get; set; }
+        void Print();
+    }
+
+    interface IDraw
+    {
+        void Draw();
+    }
+
+    class Test : ISomeInterface, IDraw
+    {
+        public int property { get; set; }
+        public void Print()
+        {
+            Console.WriteLine("Hello, World");
+        }
+        public void Draw()
+        {
+            Console.WriteLine("Draw message");
+        }
+    }
+
+        class Test2 : ISomeInterface
+        {
+            public int property { get; set; }
+            public void Print()
+            {
+                Console.WriteLine("This is invoker thug life:)");
+            }
+        }
+
+    
+    class MainClass
+    {
+        static void Main(string[] args) //метод срабатывает при старте программы
+        {
+           Test liora = new Test();
+           Test2 liora1 = new Test2();
+           liora.property = 12;
+           liora.Print();
+           liora.Draw();
+           Console.WriteLine(liora.property);
+           Console.WriteLine();
+
+           liora1.property = 25;
+           liora1.Print();
+           
+           Console.WriteLine(liora1.property);
+            Console.ReadKey();
+        }      
+    }
+}
